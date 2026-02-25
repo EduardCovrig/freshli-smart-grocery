f5c067cbaf5f7f453d1e1b810c614d630c2ec086
Eduard <eduuardcovrig@gmail.com>
2026-02-25 18:57:48 +0200

    Add notification in-memory classes to java api

--- back/src/main/java/covrig/eduard/project/Services/NotificationService.java
+++ back/src/main/java/covrig/eduard/project/Services/NotificationService.java
@@ -1,23 +1,55 @@
 package covrig.eduard.project.Services;
 
 import covrig.eduard.project.Models.Order;
+import covrig.eduard.project.dtos.notification.NotificationDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.sql.Array;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 @Slf4j
 public class NotificationService {
-    public void sendOrderConfirmation(String email, Order order) {
-        // Poate implementez altadata JavaMailSender
-        //momentan, facem doar log-uri in consola
-        log.info("----------------------------------------------------------------");
-        log.info("EMAIL SENT TO: {}", email);
-        log.info("SUBJECT: Confirmare Comandă #{}", order.getId());
-        log.info("BODY: Salut! Comanda ta în valoare de {} RON a fost confirmata.", order.getTotalPrice());
-        log.info("----------------------------------------------------------------");
+    // "BAZA DE DATE" IN-MEMORY PENTRU NOTIFICARI
+    // Cheia (Long) este ID-ul utilizatorului, Valoarea (List) este lista lui de notificari
+    private final Map<Long, List<NotificationDTO>> userNotifications = new ConcurrentHashMap<>();
+    private long notificationIdCounter = 1;
+
+    //metoda pt admin
+    public void sendPromoNotification(Long userId,String message) {
+        userNotifications.putIfAbsent(userId, new ArrayList<>());
+        NotificationDTO notification = new NotificationDTO(
+                notificationIdCounter++,
+                userId,
+                message,
+                Instant.now(),
+                false
+
+        );
+        userNotifications.get(userId).add(0, notification); //adaugam pe pozitia 0, sa fie prima.
+        log.info("Notificare In-Memory salvata pentru User ID {}: {}", userId, message);
     }
-    public void sendWelcomeEmail(String email) {
-        log.info("EMAIL SENT TO: {}", email);
-        log.info("SUBJECT: Bine ai venit pe platforma EdwC Store!");
+
+        //Metoda pt Client: isi cere automat notificarile la incarcarea paginii
+        public List<NotificationDTO> getUserNotifications(Long userId) {
+            return userNotifications.getOrDefault(userId, new ArrayList<>()); //array gol daca nu are.
+        }
+
+    //Metoda pt Client: Marcheaza o notificare ca fiind citita
+    public void markAsRead(Long userId, Long notificationId) {
+        List<NotificationDTO> notifs = userNotifications.get(userId);
+        if (notifs != null) {
+            for (NotificationDTO n : notifs) {
+                if (n.getId().equals(notificationId)) {
+                    n.setRead(true);
+                    break;
+                }
+            }
+        }
     }
 }
