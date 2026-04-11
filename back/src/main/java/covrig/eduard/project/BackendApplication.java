d86e53421c57086429a78900bb85a97a60588154
Eduard <eduuardcovrig@gmail.com>
2026-04-11 20:06:16 +0300

    enable async on backend

--- back/src/main/java/covrig/eduard/project/BackendApplication.java
+++ back/src/main/java/covrig/eduard/project/BackendApplication.java
@@ -2,10 +2,12 @@ package covrig.eduard.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling //cron jobs
+@EnableAsync
 public class BackendApplication {
 
 	public static void main(String[] args) {
